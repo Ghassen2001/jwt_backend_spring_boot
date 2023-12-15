6ccb2b0a6df74c7ce4922be8c673a573d9b58cdf
elabed ghassen <elabedghassenelabed@gmail.com>
2023-12-15 10:14:24 +0100

    backend_jwt

---
+++ src/main/java/com/ghassen/jwt/entity/JwtRequest.java
@@ -0,0 +1,23 @@
+package com.ghassen.jwt.entity;
+
+public class JwtRequest {
+
+    private String email;
+    private String userPassword;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
---
+++ src/test/java/com/ghassen/jwt/JwtYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghassen.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtYoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
